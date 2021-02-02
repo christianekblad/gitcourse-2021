0301f35252b37f78f227a50debb99f4283fbc045
christianekblad <christian.ekblad.mailbox@gmail.com>
2021-02-02 13:07:37 +0100

    skapat koden

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        List<String> listA = new ArrayList<>();
+        listA.add("Rick and morty");
+        listA.add("South park");
+        listA.add("The Mandalorian");
+        listA.add("Breaking Bad");
+        listA.add("Alternate Carbon");
+
+        String shortest = shortestString(listA);
+        System.out.println(shortest);
+    }
+
+    public static String shortestString(List<String> listA) {
+        String shortest = listA.get(0);
+
+        for(String str : listA) {
+            if (str.length() < shortest.length()) {
+                shortest = str;
+            }
+        }
+        return shortest;
     }
 }
